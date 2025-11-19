637c33c9d6f53603befcf80d88b549980f434e10
jhonfredyr <riveracjhonf@gmail.com>
2025-11-19 13:34:43 -0500

    cypress

--- src/componentes/UserList.tsx
+++ src/componentes/UserList.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react"
 
 
 const UserList:React.FC=()=>{
-    const guardar=()=>[
+    const [name,setName]=useState("");
+    const [email,setEmail]=useState("");
+    const guardar=()=>{
         fetch("https://skojryaxbquqtwvuyhfv.supabase.co",{
             method:"POST",
                headers: {
@@ -11,16 +14,17 @@ const UserList:React.FC=()=>{
               authorization:
                 "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrb2pyeWF4YnF1cXR3dnV5aGZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MTQ0MTUsImV4cCI6MjA3MzA5MDQxNX0.nZMSWKNIve_UmSe1KEehy9ocL2FIR25QflnccDRQ998",
             },
+            body:JSON.stringify({name,email})
         })
-    ]
+    }
     return(
         <div>
             <h1>Crear Usuarios</h1>
             <form onSubmit ={guardar()}>
                 <label htmlFor="name">Nombre</label>
-                <input type="text" id="name" name="name" />
+                <input type="text" id="name" name="name" value={name} onChange={(e)=>setName(e.target.value)}/>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" name="email" />
+                <input type="text" id="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
                 <button type="submit">Crear</button>
             </form>
         </div>
